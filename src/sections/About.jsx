70c4a7b8fcce13438e7b04e3b3acc88f94fa6485
rifqi011 <rifqibanusafingi11@gmail.com>
2024-12-31 11:42:20 +0700

    about update

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,21 +1,30 @@
 import Section from "./Section"
 
 export default function About() {
-    return (
+	const AboutData = [
+		{
+			quantity: 2000,
+			text: "Happy Customers",
+		},
+		{
+			quantity: 70,
+			text: "Top Hotels",
+		},
+		{
+			quantity: 200,
+			text: "Experienced Guide",
+		},
+	]
+
+	return (
 		<Section id="about" title={"about us"} description={"We are passionate team of travel enthusiasts dedicated to making your travel dreams come true. Our mission is to provide you with best travel experiences"}>
 			<div className="flex justify-between items-center pt-4">
-				<div className="text-center">
-					<h4 className="text-2xl">200+</h4>
-					<p>Happy Customers</p>
-				</div>
-				<div className="text-center">
-					<h4 className="text-2xl">70+</h4>
-					<p>Top Hotels</p>
-				</div>
-				<div className="text-center">
-					<h4 className="text-2xl">300+</h4>
-					<p>Experienced Guide</p>
-				</div>
+				{AboutData.map((data, index) => (
+					<div key={index} className="text-center">
+						<h4 className="text-2xl">{data.quantity}+</h4>
+						<p>{data.text}</p>
+					</div>
+				))}
 			</div>
 		</Section>
 	)
